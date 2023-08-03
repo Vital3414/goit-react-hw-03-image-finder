1a8a02a628062d7971929efebb98faa44052cc9c
Vital3414 <qwer1234432148@gmail.com>
2023-08-03 20:43:49 +0300

    upd searchbar

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import css from '../styles.module.css';
 
-export default class Searchbar extends React.Component {
+export default class Serchbar extends React.Component {
   state = {
     searchQuery: '',
   };
 
-  handleQueryChange = event => {
+  handelQueryChenge = event => {
     this.setState({ searchQuery: event.currentTarget.value.toLowerCase() });
   };
 
-  handleSubmit = event => {
+  handelSubmit = event => {
     event.preventDefault();
 
     if (this.state.searchQuery.trim() === '') {
-      return alert('Введіть ваш запит');
+      return alert('Введите ваш запрос');
     }
 
     this.props.onSubmit(this.state.searchQuery);
@@ -24,9 +24,9 @@ export default class Searchbar extends React.Component {
   render() {
     return (
       <header className={css.Searchbar}>
-        <form className={css.SearchForm} onSubmit={this.handleSubmit}>
-          <button type="button" className={css.SearchFormButton}>
-            <span className={css.SearchFormLabel}>&#128270;</span>
+        <form className={css.SearchForm} onSubmit={this.handelSubmit}>
+          <button type="submit" className={css.SearchFormButton}>
+            <span className={css.SearchFormButtonLabel}>&#128270;</span>
           </button>
 
           <input
@@ -35,8 +35,8 @@ export default class Searchbar extends React.Component {
             autoComplete="off"
             autoFocus
             placeholder="Search images and photos"
-            onChange={this.handleQueryChange}
-          ></input>
+            onChange={this.handelQueryChenge}
+          />
         </form>
       </header>
     );
